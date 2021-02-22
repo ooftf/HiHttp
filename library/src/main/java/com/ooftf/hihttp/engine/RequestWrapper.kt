b2461879799915dd07bd6bcd451accfb07ad0b5b
ooftf <li994749769@gmail.com>
2021-02-22 10:41:51 +0800

    优化

--- library/src/main/java/com/ooftf/hihttp/engine/RequestWrapper.kt
+++ library/src/main/java/com/ooftf/hihttp/engine/RequestWrapper.kt
@@ -1,6 +1,7 @@
 package com.ooftf.hihttp.engine
 
 import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okio.Buffer
 import okio.BufferedSink
 
@@ -32,11 +33,7 @@ class RequestWrapper(val request: Request) {
 
     fun getFormBodyParam(): MutableMap<String, String> {
         val formBody = request.body as FormBody
-        val result = LinkedHashMap<String, String>()
-        for (i in 0 until formBody.size) {
-            result[formBody.name(i)] = formBody.value(i)
-        }
-        return result
+        return getFormBodyParam(formBody)
     }
 
     fun getHttpUrl(): HttpUrl {
@@ -64,9 +61,10 @@ class RequestWrapper(val request: Request) {
     }
 
     fun getJsonBodyString(): String {
-        val buffer = Buffer()
-        request.body?.writeTo(buffer)
-        return buffer.readByteString().utf8()
+        request.body?.let {
+            return getJsonBodyString(it)
+        }
+        return ""
     }
 
 
@@ -87,33 +85,58 @@ class RequestWrapper(val request: Request) {
     }
 
     fun Request.Builder.setFormBody(param: Map<String, String>): Request.Builder {
-        val builder = FormBody.Builder()
-        param.forEach {
-            builder.add(it.key, it.value)
-        }
-        val newRequestBody = builder.build()
-        this.method(request.method, newRequestBody)
+        this.method(request.method, buildFormBody(param))
         return this
     }
 
-    fun Request.Builder.setHeader(header: Map<String, String>): Request.Builder  {
+
+    fun Request.Builder.setHeader(header: Map<String, String>): Request.Builder {
         header.forEach {
             this.header(it.key, it.value)
         }
         return this
     }
 
-    fun Request.Builder.setJsonBody(jsonBody: String): Request.Builder {
-        val newRequestBody = object : RequestBody() {
-            override fun contentType(): MediaType? {
-                return request.body?.contentType()
-            }
 
-            override fun writeTo(sink: BufferedSink) {
-                sink.buffer.writeUtf8(jsonBody)
+    fun Request.Builder.setJsonBody(jsonBody: String): Request.Builder {
+        this.method(request.method, buildJsonBody(jsonBody))
+        return this
+    }
+
+    companion object {
+        fun buildJsonBody(jsonBody: String): RequestBody {
+            return object : RequestBody() {
+                override fun contentType(): MediaType? {
+                    return "application/json;charset=utf-8".toMediaTypeOrNull()
+                }
+
+                override fun writeTo(sink: BufferedSink) {
+                    sink.buffer.writeUtf8(jsonBody)
+                }
             }
         }
-        this.method(request.method, newRequestBody)
-        return this
+
+        fun buildFormBody(param: Map<String, String>): FormBody {
+            val builder = FormBody.Builder()
+            param.forEach {
+                builder.add(it.key, it.value)
+            }
+            val newRequestBody = builder.build()
+            return newRequestBody
+        }
+
+        fun getFormBodyParam(formBody: FormBody): MutableMap<String, String> {
+            val result = LinkedHashMap<String, String>()
+            for (i in 0 until formBody.size) {
+                result[formBody.name(i)] = formBody.value(i)
+            }
+            return result
+        }
+
+        fun getJsonBodyString(body: RequestBody): String {
+            val buffer = Buffer()
+            body.writeTo(buffer)
+            return buffer.readByteString().utf8()
+        }
     }
 }
