8077417442d979ec317376d6844f1697d1326025
ooftf <li994749769@gmail.com>
2021-03-26 18:12:09 +0800

    sync

--- app/src/main/java/com/ooftf/hihttp/sample/MainActivity.kt
+++ app/src/main/java/com/ooftf/hihttp/sample/MainActivity.kt
@@ -1,12 +1,66 @@
 package com.ooftf.hihttp.sample
 
+import android.Manifest
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.os.Environment
+import android.util.Log
+import androidx.fragment.app.FragmentActivity
+import com.ooftf.hihttp.download.DownLoadEngine
+import com.ooftf.hihttp.download.ProgressCallback
 import com.ooftf.sample.sample.R
-class MainActivity : AppCompatActivity() {
+import com.tbruyelle.rxpermissions3.RxPermissions
+import okhttp3.Call
+import okhttp3.Dispatcher
+import okhttp3.OkHttpClient
+import java.io.File
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.random.Random
+
+class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        RxPermissions(this)
+                .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe {
+                    if (it) {
+                        sss()
+                    }
+                }
+        //https://cpcti.oss-cn-beijing.aliyuncs.com/apk/IOrderAB.apk
+    }
+
+    private fun sss() {
+        var start = AtomicInteger(0)
+        var success = AtomicInteger(0)
+        var error = AtomicInteger(0)
+        val build = OkHttpClient.Builder().dispatcher(Dispatcher(ThreadPoolExecutor(1, 1, 100, TimeUnit.SECONDS, LinkedBlockingQueue()))).build()
+        for (i in 0 until 100) {
+            var file = File(Environment.getExternalStorageDirectory(), "${System.currentTimeMillis() + Random.nextInt()}.apk")
+            DownLoadEngine.download(build, "https://cpcti.oss-cn-beijing.aliyuncs.com/apk/IOrderAB.apk", file, object : ProgressCallback.DownLoadProgressListener {
+                override fun start(call: Call?, totalByte: Long) {
+                    Log.e("start", "${start.incrementAndGet()}")
+                }
+
+                override fun progress(call: Call?, bytesRead: Long, contentLength: Long) {
+                    //Log.e("task::" + i, "${bytesRead.toDouble() / contentLength}")
+                }
+
+                override fun success(call: Call?, file: File?) {
+                    Log.e("success", "${success.incrementAndGet()}")
+
+                }
+
+                override fun fail(call: Call?, e: Exception?) {
+                    Log.e("error", "${error.incrementAndGet()}")
+                    e?.printStackTrace()
+                }
+
+            })
+        }
     }
 }
--- library/src/main/java/com/ooftf/hihttp/download/DownLoadEngine.java
+++ library/src/main/java/com/ooftf/hihttp/download/DownLoadEngine.java
@@ -3,6 +3,7 @@ package com.ooftf.hihttp.download;
 import java.io.File;
 
 import okhttp3.Call;
+import okhttp3.Dispatcher;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
@@ -16,6 +17,13 @@ public class DownLoadEngine {
     public static Call download(String url, File file, ProgressCallback.DownLoadProgressListener listener) {
         Request request = new Request.Builder().url(url).get().build();
         Call call = new OkHttpClient.Builder().build().newCall(request);
+        call.enqueue(new ProgressCallback(file, listener));
+        return call;
+    }
+
+    public static Call download(OkHttpClient okHttpClient,String url, File file, ProgressCallback.DownLoadProgressListener listener) {
+        Request request = new Request.Builder().url(url).get().build();
+        Call call = okHttpClient.newCall(request);
         call.enqueue(new ProgressCallback(file, listener));
         return call;
     }
